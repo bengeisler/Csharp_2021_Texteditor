61e680181ec8facdef6b12a52d5905d23aa4523d
Benedikt Geisler <benedikt.geisler@posteo.de>
2021-02-18 09:01:39 +0100

    Projektdateien hinzufügen.

---
+++ Csharp_2021_Texteditor/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿
+namespace Csharp_2021_Texteditor
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Erforderliche Designervariable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Verwendete Ressourcen bereinigen.
+		/// </summary>
+		/// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Vom Windows Form-Designer generierter Code
+
+		/// <summary>
+		/// Erforderliche Methode für die Designerunterstützung.
+		/// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.txtEditor = new System.Windows.Forms.TextBox();
+			this.btnÖffnen = new System.Windows.Forms.Button();
+			this.btnSpeichern = new System.Windows.Forms.Button();
+			this.btnBeenden = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// txtEditor
+			// 
+			this.txtEditor.Location = new System.Drawing.Point(13, 13);
+			this.txtEditor.Multiline = true;
+			this.txtEditor.Name = "txtEditor";
+			this.txtEditor.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+			this.txtEditor.Size = new System.Drawing.Size(510, 337);
+			this.txtEditor.TabIndex = 0;
+			// 
+			// btnÖffnen
+			// 
+			this.btnÖffnen.Location = new System.Drawing.Point(13, 357);
+			this.btnÖffnen.Name = "btnÖffnen";
+			this.btnÖffnen.Size = new System.Drawing.Size(75, 23);
+			this.btnÖffnen.TabIndex = 1;
+			this.btnÖffnen.Text = "Öffnen";
+			this.btnÖffnen.UseVisualStyleBackColor = true;
+			this.btnÖffnen.Click += new System.EventHandler(this.btnÖffnen_Click);
+			// 
+			// btnSpeichern
+			// 
+			this.btnSpeichern.Location = new System.Drawing.Point(95, 356);
+			this.btnSpeichern.Name = "btnSpeichern";
+			this.btnSpeichern.Size = new System.Drawing.Size(75, 23);
+			this.btnSpeichern.TabIndex = 2;
+			this.btnSpeichern.Text = "Speichern";
+			this.btnSpeichern.UseVisualStyleBackColor = true;
+			this.btnSpeichern.Click += new System.EventHandler(this.btnSpeichern_Click);
+			// 
+			// btnBeenden
+			// 
+			this.btnBeenden.Location = new System.Drawing.Point(448, 356);
+			this.btnBeenden.Name = "btnBeenden";
+			this.btnBeenden.Size = new System.Drawing.Size(75, 23);
+			this.btnBeenden.TabIndex = 3;
+			this.btnBeenden.Text = "Beenden";
+			this.btnBeenden.UseVisualStyleBackColor = true;
+			this.btnBeenden.Click += new System.EventHandler(this.btnBeenden_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(537, 391);
+			this.Controls.Add(this.btnBeenden);
+			this.Controls.Add(this.btnSpeichern);
+			this.Controls.Add(this.btnÖffnen);
+			this.Controls.Add(this.txtEditor);
+			this.Name = "Form1";
+			this.Text = "Ein einfacher Editor";
+			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox txtEditor;
+		private System.Windows.Forms.Button btnÖffnen;
+		private System.Windows.Forms.Button btnSpeichern;
+		private System.Windows.Forms.Button btnBeenden;
+	}
+}
+
---
+++ Csharp_2021_Texteditor/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Csharp_2021_Texteditor
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void btnSpeichern_Click(object sender, EventArgs e)
+		{
+			try
+			{
+				// SaveFileDialog erstellen
+				var sfd = new SaveFileDialog()
+				{
+					InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments),
+					Filter = "Textdateien (*.txt)|*.txt",
+					Title = "Datei speichern"
+				};
+
+				// SaveFileDialog anzeigen & Rückgabe auswerten => Datei speichern
+				if (sfd.ShowDialog() == DialogResult.OK) 
+					File.WriteAllText(sfd.FileName, txtEditor.Text);
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message);
+			}
+		}
+
+		private void btnÖffnen_Click(object sender, EventArgs e)
+		{
+			try
+			{
+				// OpenFileDialog erstellen
+				var ofd = new OpenFileDialog()
+				{
+					InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments),
+					Filter = "Textdateien (*.txt)|*.txt",
+					Title = "Datei öffnen"
+				};
+
+				// OpenFileDialog anzeigen, Rückgabe auswerten => Datei lesen
+				if (ofd.ShowDialog() == DialogResult.OK)
+					txtEditor.Text = File.ReadAllText(ofd.FileName);
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message);
+			}
+		}
+
+		private void btnBeenden_Click(object sender, EventArgs e)
+		{
+			var abfrageBeenden = MessageBox.Show("Wollen Sie wirklich beenden?", "Beenden",
+				MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+
+			if (abfrageBeenden == DialogResult.OK) Close();
+		}
+
+		private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+		{
+			var abfrageBeenden = MessageBox.Show("Wollen Sie wirklich beenden?", "Beenden",
+				MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+
+			if (abfrageBeenden == DialogResult.Cancel) e.Cancel = true;
+		}
+	}
+}
